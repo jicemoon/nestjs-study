bda108d99722603bf70461d60c2739286ee06c30
jicemoon <jicemoon@outlook.com>
2019-10-08 15:18:10 +0800

    重命名报名

--- server/src/configs/const.define.ts
+++ server/src/configs/const.define.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path';
 
+// const DB_USER = 'reese';
+// const DB_PW = 'TE09bRkDM7tH1HAn';
+// const DB_NAME = 'NestStudy';
+// export const DB_URI: string = `mongodb+srv://${DB_USER}:${DB_PW}@cluster0-1elaz.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 export const DB_URI: string = 'mongodb://localhost:27017/NestHello';
-const DB_USER = 'reese';
-const DB_PW = 'TE09bRkDM7tH1HAn';
-const DB_NAME = 'NestStudy';
 export const SALT_ROUNTS: number = 15;
 export const SECRET_KEY: string = 'eiujskdf@#$8*(sdkjfksadf';
 export const EXPIRES_IN: number = 24 * 60 * 60;
